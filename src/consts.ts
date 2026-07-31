d2dfc3a2c575cb93ea19bdd18df1c3f731e4bc6c
紫云 <1583931339@qq.com>
2026-07-31 09:09:02 +0800

    Update site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'ZiYyun Blog';
-export const SITE_DESCRIPTION = '发布和整理 Obsidian Markdown 笔记的个人博客。';
+export const SITE_DESCRIPTION = '咕咕嘎嘎';
